64a9ae5185d05d0ea1ff44766a697d1dba9eddb6
Mateo Carballo Alonso <a22mateoca@ATEIS.LOCAL>
2023-04-26 17:41:41 +0200

    Creando test

---
+++ 02_Segundo Trimestre/RefactorizacionTesteo/src/test/java/org/example/MetodosParaTestearTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class MetodosParaTestearTest extends TestCase {
+
+
+
+    public void testCalcularDivision() throws Exception {
+        MetodosParaTestear m = new MetodosParaTestear();
+        assertEquals(3.4f,m.calcularDivision(8.5f, 2.5f));
+    }
+
+    public void testFactorial() throws Exception {
+        MetodosParaTestear m = new MetodosParaTestear();
+        assertEquals(24f, m.factorial((byte)4));
+    }
+
+    public void testBusca() {
+        char[] arrayC={'z','x','y','a','b'};
+        MetodosParaTestear m = new MetodosParaTestear();
+        assertTrue(m.busca('a', arrayC));
+    }
+
+    public void testObtenerAcronimo() {
+    }
+}
